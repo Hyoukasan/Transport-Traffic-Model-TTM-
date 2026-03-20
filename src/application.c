4f185d9e25807ddd0ec91186f6e03e57e261d202
HyoukaSan <keynotyn13@mail.ru>
2026-03-21 02:39:32 +0300

    update

--- src/application.c
+++ src/application.c
@@ -7,9 +7,9 @@
 #include "menu.h"
 
 static GLFWwindow* window = NULL;
-
-static AppState app_state = APP_STATE_MAIN_MENU;
-static Menu* menu = {0};
+static app_state = APP_STATE_MAIN_MENU;
+static Menu* menu;
+static int prev_lmb = GLFW_RELEASE;
 
 int application_init(int width, int height, const char *title){
 
@@ -34,14 +34,27 @@ int application_init(int width, int height, const char *title){
         return 1;
     }
 
+    menu_init(menu);
+
     return 0;
 }
 
 bool application_is_running(void){
-    return !glfwWindowShouldClose(window);
+    return !glfwWindowShouldClose(window) && app_state != APP_STATE_EXIT;
 }
 
 void application_update(void){
+    double mx, my;
+    glfwGetCursorPos(window, &mx, &my);
+    
+    int lmb = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
+    bool click = (lmb == GLFW_PRESS && prev_lmb == GLFW_RELEASE);
+    prev_lmb = lmb;
+
+    if(app_state == APP_STATE_MAIN_MENU || app_state == APP_STATE_SETTINGS_MENU){
+        menu_update(menu, (int)mx, (int)my, click);
+    }
+
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     glfwSwapBuffers(window);
--- src/menu.c
+++ src/menu.c
@@ -1,1 +1,32 @@
 #include "menu.h"
+
+void menu_init(Menu *menu){
+    menu->current_state = APP_STATE_MAIN_MENU;
+    menu->button_count = 0;
+    menu->selected_index = -1;
+}
+
+static void menu_load_state(Menu *menu, AppState app_state){
+    switch(app_state){
+        case APP_STATE_MAIN_MENU:
+            menu->buttons[0] = (Button){};
+            menu->buttons[1] = (Button){};
+            menu->button_count =2;
+            break;
+        case APP_STATE_SETTINGS_MENU:
+            menu->buttons[0] = (Button){};
+            menu->buttons[1] = (Button){};
+            menu->button_count =2;
+            break;
+
+        default: break;
+    }   
+}
+
+void menu_update(Menu* menu, int mx, int my, bool click){
+    
+    for(int i = 0; i < menu->button_count; i++){
+        Button *button = &menu->buttons[i];
+    }
+
+}
