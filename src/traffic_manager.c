ab4dc0515d3a787436dc6cd3c91549d3fcafdef2
HyoukaSan <keynotyn13@mail.ru>
2026-04-24 00:49:56 +0300

    rebuild EVA

--- src/traffic_manager.c
+++ src/traffic_manager.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 
 #include "traffic_manager.h"
 #include "traffic_config.h"
@@ -9,18 +10,25 @@
 
 
 
-TrafficManager* traffic_manager_init(TrafficManager* manager, const TrafficConfig* config) {
-    manager = (TrafficManager*)malloc(sizeof(TrafficManager));
-    if(manager== NULL || config == NULL) {
+int traffic_manager_init(TrafficManager* manager, const TrafficConfig* config) {
+    if(config == NULL) {
+        fprintf(stderr, "Config undefined!\n");
+        return -1;
+    }
+    
+    
+    manager = (TrafficManager*)calloc(1,sizeof(TrafficManager));
+    if(manager == NULL) {
         fprintf(stderr, "Manager initialization failed!\n");
-        return NULL;
+        return -1;
     }
 
     manager->max_cars = config->max_cars;
     manager->cars = (Car*)malloc((size_t)manager->max_cars * sizeof(Car));
-    if(manager->cars = NULL) {
+    if(manager->cars == NULL) {
         fprintf(stderr, "Cars initialization failed!\n");
-        return NULL;
+        free(manager);
+        return -1;
     }
     
     manager->accident_count = 16;
@@ -28,7 +36,8 @@ TrafficManager* traffic_manager_init(TrafficManager* manager, const TrafficConfi
     if(manager->accidents == NULL) {
         fprintf(stderr, "Accidents initialization failed!\n");
         free(manager->cars);
-        return NULL;
+        free(manager);
+        return -1;
     }
 
     manager->graph = graph_create(1920, 1080, 50, 1);
@@ -36,7 +45,8 @@ TrafficManager* traffic_manager_init(TrafficManager* manager, const TrafficConfi
         fprintf(stderr, "Graph initialization failed!\n");
         free(manager->cars);
         free(manager->accidents);
-        return NULL;
+        free(manager);
+        return -1;
     }
 
     manager->car_count = 0;
@@ -45,5 +55,5 @@ TrafficManager* traffic_manager_init(TrafficManager* manager, const TrafficConfi
     manager->time = 0.0f;
     manager->next_car_id = 0; 
 
-    return manager;
+    return 0;
 }
