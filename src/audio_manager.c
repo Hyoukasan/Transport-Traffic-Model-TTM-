379a54bda4da3b1c6417ea8184289825d5a141d6
HyoukaSan <keynotyn13@mail.ru>
2026-04-25 02:01:17 +0300

    MAXIMUM RENDERING

--- src/audio_manager.c
+++ src/audio_manager.c
@@ -4,35 +4,60 @@
 #include "miniaudio.h"
 
 
-static ma_engine engine;
-static ma_sound menu_music;
-static ma_sound simulation_music;
-static ma_sound click;
-
-static AudioType audio = {0};
+static AudioManager audio = {0};
 
 int audio_init(void) {
-    return ma_engine_init(NULL, &engine) == MA_SUCCESS;
+    return ma_engine_init(NULL, &audio.engine) == MA_SUCCESS;
 }
 
 void audio_shutdown(void) {
-    ma_engine_uninit(&engine);
+    ma_engine_uninit(&audio.engine);
 }
 
 
 void audio_play_click(void) {
 
-}
+    if(audio.initialized != 0) {
+        if(ma_sound_init_from_file(&audio.engine, "Data/audio/click.mp3", 
+            0, NULL, NULL, &audio) != MA_SUCCESS) {
+            return;
+        }
 
+        ma_sound_set_looping(&audio.click, MA_TRUE);
+        ma_sound_set_volume(&audio.click, 0.6f);
+    }
+
+    ma_sound_start(&audio.click);
+}
 
 void audio_start_menu_music(void) {
 
+    if(audio.initialized != 0) {
+        if(ma_sound_init_from_file(&audio.engine, "Data/audio/menu.mp3", 
+            MA_SOUND_FLAG_STREAM, NULL, NULL, &audio.menu_music) != MA_SUCCESS) {
+            return;
+        }
+
+        ma_sound_set_looping(audio.menu_music, MA_TRUE);
+        ma_sound_set_volume(audio.menu_music, 0.4f);
+    }
+
+    ma_sound_start(audio.menu_music);
 }
 
 void audio_start_simulation_music(void) {
+    if(audio_init() == 0) {
+        return;
+    }
 
 }
 
 void audio_stop_music(void) {
+    if(audio.menu_music_loaded != 0) {
 
+    }
+
+    if(audio.simulation_music_loaded != 0) {
+
+    }
 }
--- src/audio_manager.h
+++ src/audio_manager.h
@@ -1,11 +1,17 @@
 #ifndef AUDIO_MANAGER_H
 #define AUDIO_MANAGER_H
 
-typedef enum {
-    AUDIO_MAIN_MENU,
-    AUDIO_SIMULATION,
-    AUDIO_CKICK
-} AudioType;
+typedef struct {
+    ma_engine engine;
+    ma_sound menu_music;
+    ma_sound simulation_music;
+    ma_sound click;
+
+    int initialized;
+    int menu_music_loaded;
+    int simulation_music_loaded;
+
+} AudioManager;
 
 int audio_init(void);
 void audio_shutdown(void);
