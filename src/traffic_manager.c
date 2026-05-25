0cd4ccb3055ecac41d69f6e2fc8bc3db10da69cd
HyoukaSan <keynotyn13@mail.ru>
2026-05-25 20:34:59 +0300

    fix rtaffic

--- src/traffic_manager.c
+++ src/traffic_manager.c
@@ -1362,21 +1362,6 @@ bool traffic_manager_add_accident_on_selected_lane(TrafficManager* manager) {
     return true;
 }
 
-const char* get_state_name(CarState state) {
-    switch (state) {
-        case CAR_STATE_NORMAL:          return "NORMAL";
-        case CAR_STATE_BRAKING:         return "BRAKING";
-        case CAR_STATE_SLOWING:         return "SLOWING";
-        case CAR_STATE_OVERTAKING:      return "OVERTAKING";
-        case CAR_STATE_ACCIDENT:        return "ACCIDENT";
-        case CAR_STATE_LANE_CHANGE:     return "LANE_CHANGE";
-        case CAR_STATE_TURNING:         return "TURNING";
-        case CAR_STATE_TRAFFIC_LIGHT:   return "TRAFFIC_LIGHT";
-        case CAR_STATE_INTERSECTION_WAIT: return "WAITING";
-        default:                        return "UNKNOWN";
-    }
-}
-
 int traffic_manager_update(TrafficManager *manager, float dt) {
     if (manager == NULL || manager->graph == NULL) {
         return -1;
@@ -1395,30 +1380,6 @@ int traffic_manager_update(TrafficManager *manager, float dt) {
 
     for (int i = 0; i < manager->car_count; i++) {
         Car* car = &manager->cars[i];
-
-
-        char line[256];
-        float x_offset = 400.0f;
-        float y_offset = 50.0f;
-        float step = 35.0f;
-
-        snprintf(line, sizeof(line), "State: %s | Pos: %.2f", 
-                 get_state_name(car->state), car->position);
-
-        renderer_draw_text(x_offset + 2, y_offset + 2, line, 2.0f, 0.0f, 0.0f, 0.0f, 1920, 1080);
-        renderer_draw_text(x_offset, y_offset, line, 2.0f, 1.0f, 1.0f, 1.0f, 1920, 1080);
-            
-        snprintf(line, sizeof(line), "Speed: %.2f | Turn: %d", 
-             car->speed, car->turn_decided);
-
-        renderer_draw_text(x_offset + 2, y_offset + step + 2, line, 2.0f, 0.0f, 0.0f, 0.0f, 1920, 1080);
-        renderer_draw_text(x_offset, y_offset + step, line, 2.0f, 1.0f, 1.0f, 1.0f, 1920, 1080);
-
-        snprintf(line, sizeof(line), "CrossedIdx: %d | TargetRoad: %d", 
-                    car->last_turn_x, car->turn_target_road_id); // Используй свои переменные для crossed.idx
-
-        renderer_draw_text(x_offset + 2, y_offset + (step * 2) + 2, line, 2.0f, 0.0f, 0.0f, 0.0f, 1920, 1080);
-        renderer_draw_text(x_offset, y_offset + (step * 2), line, 2.0f, 1.0f, 1.0f, 1.0f, 1920, 1080);
 
         if (car->state != CAR_STATE_TRAFFIC_LIGHT) {
             if(!traffic_manager_update_overtake_return(manager, car)) {
